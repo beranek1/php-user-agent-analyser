f0e0e2e24e96cd0b52e2c35b65951196db5b65fc
beranek1 <52349197+beranek1@users.noreply.github.com>
2019-07-14 19:23:10 +0200

    Add os and device detection

--- uaa.php
+++ uaa.php
@@ -10,7 +10,7 @@
 */
 
 function analyse_user_agent($user_agent) {
-    $result = array("browser" => array("name" => null, "version" => null));
+    $result = array("browser" => array("name" => null, "version" => null), "os" => array("name" => null, "version" => null), "device" => array("name" => null));
     $gecko = false;
     if(preg_match("/Mozilla\/\d[\d.]* \([A-Za-z0-9_. ;:]*\) Gecko\/\d+/i", $user_agent)) {
         $gecko = true;
@@ -32,21 +32,59 @@ function analyse_user_agent($user_agent) {
         $result["browser"]["name"] = $browser[0];
         $result["browser"]["version"] = $browser[1];
     }
-    if(preg_match("/\([A-Za-z0-9_. ;:\/]*\)/", $user_agent, $match)) {
+    if(preg_match("/\([A-Za-z0-9_.\- ;:\/]*\)/", $user_agent, $match)) {
         $platform = preg_replace("/\(/", "", $match[0]);
         $platform = preg_replace("/\)/", "", $platform);
         $platforms = preg_split("/; /", $platform);
+        $os = null;
+        $osv = null;
         if($trident) {
             $browser = preg_split("/ /",$platforms[1]);
             if(preg_match("/msie/i", $browser[0])) {
                 $result["browser"]["name"] = $browser[0];
                 $result["browser"]["version"] = $browser[1];
+                $os = preg_split("/ \d/", preg_replace("/ nt/i", "",$platforms[2]));
+                $osv = preg_split("/ /",$platforms[2]);
             } else {
                 $result["browser"]["name"] = "msie";
                 $version = preg_split("/:/", $platforms[array_key_last($platforms)]);
                 $result["browser"]["version"] = $version[1];
             }
         }
+        if(preg_match("/windows/i", $platforms[0])) {
+            $os = preg_split("/ \d/", preg_replace("/ nt/i", "",$platforms[0]));
+            $osv = preg_split("/ /",$platforms[0]);
+            $result["device"]["name"] = "pc";
+            if(preg_match("/phone/i", $os[0])) {
+                $result["device"]["name"] = $platforms[array_key_last($platforms)-1]." ".$platforms[array_key_last($platforms)];
+            }
+            if(isset($platforms[3]) && preg_match("/xbox/i", $platforms[3])) {
+                $result["device"]["name"] = $platforms[3];
+                if(isset($platforms[4])) {
+                    $result["device"]["name"] = $platforms[4];
+                }
+            }
+        } else if(preg_match("/linux/i", $platforms[0])) {
+            $os = preg_split("/ \d/",$platforms[1]);
+            $osv = preg_split("/ /",$platforms[1]);
+            if(preg_match("/android/i", $os[0]) && preg_match("/build/i", $platforms[2])) {
+                $device = preg_split("/ build/i", $platforms[2]);
+                $result["device"]["name"] = $device[0];
+            }
+        } else if(preg_match("/cros/i", $platforms[1])) {
+            $os = preg_split("/ /",$platforms[1]);
+            $result["device"]["name"] = "chromebook";
+        } else if(preg_match("/macintosh/i", $platforms[0])) {
+            $os = preg_split("/ \d/",preg_replace("/intel /i", "", $platforms[1]));
+            $osv = preg_split("/ /",$platforms[1]);
+            $result["device"]["name"] = "mac";
+        } else if(preg_match("/iphone/i", $platforms[0]) || preg_match("/ipad/i", $platforms[0]) || preg_match("/ipod/i", $platforms[0])) {
+            $os = preg_split("/ \d/",preg_replace("/cpu /i", "", $platforms[1]));
+            $osv = preg_split("/ /", preg_replace("/ like mac os x/i", "", $platforms[1]));
+            $result["device"]["name"] = $platforms[0];
+        }
+        $result["os"]["name"] = isset($os) ? $os[0] : null;
+        $result["os"]["version"] = isset($osv) ? $osv[array_key_last($osv)] : null;
     }
     return $result;
 }
